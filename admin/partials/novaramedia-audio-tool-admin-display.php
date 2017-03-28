edf9c56b3dc0941a7c3144676682954d5e6f220a
Patrick Best <patrickbest@patrickbest.com>
2017-03-28 16:59:03 -0600

    use data to set values in view

--- admin/partials/novaramedia-audio-tool-admin-display.php
+++ admin/partials/novaramedia-audio-tool-admin-display.php
@@ -34,20 +34,72 @@ $recent_posts = get_posts(array(
   <button id="get-data" class="button button-primary">Get Data</button>
 </section>
 
+<hr>
+
 <section>
   <h3>Metadata</h3>
-
-  *** here goes a replication of the podcast tool's metadata section but a correct one ***
+  <table class="form-table output">
+    <tbody>
+      <tr>
+        <th scope="row">Title:</th>
+        <td><span id="output-title" class="easy-select"></span></td>
+      </tr>
+      <tr>
+        <th scope="row">Author:</th>
+        <td><span class="easy-select">Novara Media</span></td>
+      </tr>
+      <tr>
+        <th scope="row">Album:</th>
+        <td><span class="easy-select">Novara Audio: Radio for a Different Politics</span></td>
+      </tr>
+      <tr>
+        <th scope="row">Permalink & MP3 comment tag:</th>
+        <td><span id="output-permalink" class="easy-select"></span></td>
+      </tr>
+      <tr>
+        <th scope="row">Safe filename:</th>
+        <td><span id="output-filename" class="easy-select"></span></td>
+      </tr>
+      <tr>
+        <th scope="row">Copy:</th>
+        <td><p id="output-copy" class="easy-select"></p></td>
+      </tr>
+      <tr>
+        <th scope="row">MP3 lyrics:</th>
+        <td><p id="output-lyrics" class="easy-select"></p></td>
+      </tr>
+      <tr>
+        <th scope="row">Archive.org HTML copy:</th>
+        <td><p id="output-archive-org-copy" class="easy-select"></p></td>
+      </tr>
+      <tr>
+        <th scope="row">Tags:</th>
+        <td><p id="output-tags" class="easy-select"></p></td>
+      </tr>
+      <tr>
+        <th scope="row">Actions:</th>
+        <td><a id="output-archive-org-link" class="button button-primary" target="_blank" rel="noopener">Create Archive.org upload</a></td>
+      </tr>
+    </tbody>
+  </table>
 </section>
+
+<hr>
 
 <section>
   <h3>Artwork</h3>
 
-  *** here goes a replication of the podcast tool's artwork generator ***
+  <div class="output">
+    *** here goes a replication of the podcast tool's artwork generator ***
+  </div>
 </section>
+
+<hr>
 
 <section>
   <h3>MP3 tagger</h3>
 
-  *** here goes a drag/drop UI element for tagging mp3 files ***
+  <div class="output">
+    *** here goes a drag/drop UI element for tagging mp3 files ***
+  </div>
 </section>

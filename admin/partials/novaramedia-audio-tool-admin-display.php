24d16e089aa8615af0fd44685b4256d795c3a4bb
Patrick Best <patrickbest@patrickbest.com>
2017-05-28 14:29:14 +0100

    hide on generation
    and hide output on first load

--- admin/partials/novaramedia-audio-tool-admin-display.php
+++ admin/partials/novaramedia-audio-tool-admin-display.php
@@ -89,7 +89,7 @@ $recent_posts = get_posts(array(
 <section>
   <h3>Artwork</h3>
   <canvas id="artwork-canvas" width="1200" height="1200" style="display: none;"></canvas>
-  <img id="artwork-canvas-export" />
+  <img id="artwork-canvas-export" class="output" />
 </section>
 
 <hr>
